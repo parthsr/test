426fd50e363ac5286cc615a06ca22db2b2244c27
parth <srivastavaparth2016@gmail.com>
2018-01-23 12:30:37 +0530

    first test case

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let bowling = require('./bowling');
 
-const arr = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+
 // describe('valid easy test cases', () => {
 //   test('test case 1', () => {
 //     expect(bowling(arr)).toBe(90);
@@ -19,12 +19,12 @@ const arr = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 // calculate([10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
 //
 // calculate([10, 10, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 3, 2]);
-describe('my beverage', () => {
-  test('is delicious', () => {
-    expect(bowling(arr)).toBe(90);
+describe('valid easy test cases', () => {
+  test('test case 1 without any strike spare', () => {
+    expect(bowling([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
   });
 
-  // test('is not sour', () => {
-  //   expect(myBeverage.sour).toBeFalsy();
-  // });
+  test('test case 1 with 1 strike ', () => {
+    expect(bowling([10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(100);
+  });
 });
